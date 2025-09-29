9f71a7bf46b0ad9bc96a1109de6b84eff131a0d0
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2025-09-29 11:10:22 +0200

    Push Update 29/09/2025, 11:10:22

--- setup.py
+++ setup.py
@@ -12,15 +12,30 @@ setuptools.setup(
 	name="ledsign",
 	version=VERSION,
 	description="LED Sign Python API",
+	long_description=description,
+	long_description_content_type="text/markdown",
+	author="Krzesimir Hyżyk",
 	url="https://github.com/krzem5/ledsign",
+	packages=["ledsign"],
+    python_requires=">=3.9",
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: End Users/Desktop",
+		"License :: OSI Approved :: BSD License",
+		"Natural Language :: English",
+		"Operating System :: Microsoft :: Windows",
+		"Operating System :: POSIX :: Linux",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"Programming Language :: Python :: 3.11",
+		"Programming Language :: Python :: 3.12",
+		"Programming Language :: Python :: 3.13"
+	],
+	license="BSD-3-Clause",
+	install_requires=[],
 	project_urls={
 		"Source Code": "https://github.com/krzem5/ledsign",
-	},
-	author="Krzesimir Hyżyk",
-	license="BSD-3-Clause",
-	classifiers=[],
-	install_requires=[],
-	packages=["ledsign"],
-	long_description=description,
-	long_description_content_type="text/markdown"
+		"Documentation": "https://ledsign.readthedocs.io/en/latest/",
+	}
 )
